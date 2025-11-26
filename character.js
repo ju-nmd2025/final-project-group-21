6b2c412526d0d456ac3e6036820fc89d1143949c
quanhoangindex <quanhoangindex@gmail.com>
2025-11-27 00:24:43 +0100

    change color and add shadow on character

--- character.js
+++ character.js
@@ -4,6 +4,17 @@ export let character = {
     w: 50,
     h: 50,
     draw() {
+        push();
+        //shadow
+        drawingContext.shadowBlur = 12;
+        drawingContext.shadowOffsetX = 0;
+        drawingContext.shadowOffsetY = 0;
+        drawingContext.shadowColor = "#65DD00";
+
+        //character
+        fill("#F7F704");
+        stroke("#0C0C00");
         rect(this.x, this.y, this.w, this.h);
+        pop();
     },
 };
