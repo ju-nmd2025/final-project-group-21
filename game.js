cbac98594f541f4b4af87d21e58c945661d36f3d
quanhoangindex <quanhoangindex@gmail.com>
2025-11-29 11:24:16 +0100

    finish jumpping char on platform

--- game.js
+++ game.js
@@ -14,48 +14,56 @@ function drawObstacle() {
     pop();
 }
 
-
 function draw() {
     background("#10164E");
 
-    character.draw();
-	platform.draw();
-    floor.draw();
-
     //platform movement
-    platform.x -= 10;
-    if(platform.x + platform.w < 0){
+    platform.x -= 6;
+    if (platform.x + platform.w < 0) {
         platform.x = 500;
     }
 
+    //
 
-
-    // land character
-    if(character.y + character.h < floor.y){
+    // land character n not standing on platform
+    if (
+        character.y + character.h < floor.y &&
+        !isOnPlatform(character, platform)
+    ) {
         character.y += 10;
     }
 
+    //standing exactly on platform service
+    if (isOnPlatform(character, platform)) {
+        character.y = platform.y - character.h;
+    }
+
+    character.draw();
+    platform.draw();
+    floor.draw();
 }
 
-function isOnPlatform(character,platform){
+function isOnPlatform(character, platform) {
     const charBottom = character.y + character.h;
     const platformTop = platform.y;
 
     // right side char > left side plat
     // left side char c < right side plat
     // => match width
-    const colliding = character.x + character.w > platform.x && character.x < platform.x + platform.w;
+    const colliding =
+        character.x + character.w > platform.x &&
+        character.x < platform.x + platform.w;
 
     //standing on platform
-    if(colliding && charBottom === platformTop){
-        return true
+    if (colliding && charBottom === platformTop) {
+        return true;
     }
     //else drop
     return false;
 }
 
-function mouseClicked(){
-    if(character.y + character.h === floor.y){
-        character.y -= 80;
+function mouseClicked() {
+    if (character.y + character.h === floor.y) {
+        character.y -= 100;
     }
 }
