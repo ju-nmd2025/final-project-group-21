4a3d0e430e004334e18b83529561c3170d5bab5d
stol25jr <stol25jr@student.ju.se>
2025-12-12 02:06:12 +0100

    refined text, add visual elements

--- gameover.js
+++ gameover.js
@@ -4,33 +4,22 @@ function setup() {
 
 function finishText() {
   push();
+  drawingContext.shadowBlur = 12;
+  drawingContext.shadowOffsetX = 0;
+  drawingContext.shadowOffsetY = 0;
+  drawingContext.shadowColor = "#d82000ff";
+  strokeWeight(6);
+  stroke("#d82000ff");
+  textFont("Tahoma");
   textAlign(CENTER, CENTER);
-  textSize(32);
-  fill("white");
-  text("Finish", 200, 200);
-  pop();
-}
-
-function playAgainRect() {
-  push();
-  fill(255, 255, 0);
-  noStroke();
-  rect(100, 250, 200, 50);
-  pop();
-}
-
-function playAgainText() {
-  push();
-  textAlign(CENTER, CENTER);
-  textSize(32);
-  fill(0);
-  text("Play Again", 200, 275);
+  textStyle(BOLD);
+  textSize(55);
+  fill("#ff2600ff");
+  text("GAME OVER!", 250, 200);
   pop();
 }
 
 function draw() {
   background("#10164E");
   finishText();
-  playAgainRect();
-  playAgainText();
 }
