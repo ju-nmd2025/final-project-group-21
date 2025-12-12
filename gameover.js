6436decd3f266cde3713e3204257603fb2230aa8
stol25jr <stol25jr@student.ju.se>
2025-12-12 02:45:12 +0100

    added play button element, refine

--- gameover.js
+++ gameover.js
@@ -19,7 +19,73 @@ function finishText() {
   pop();
 }
 
+function bruh() {
+  push();
+  noStroke();
+  fill("#1E105B");
+  rect(0, 520, 500, 80);
+  pop();
+}
+
+function bruhx() {
+  push();
+  stroke("#5D5094");
+  strokeWeight(6);
+  line(0, 520, 500, 520);
+  pop();
+}
+
+function btns() {
+  push();
+  drawingContext.shadowBlur = 12;
+  drawingContext.shadowOffsetX = 0;
+  drawingContext.shadowOffsetY = 0;
+  drawingContext.shadowColor = "#050505ff";
+  noStroke();
+  fill("#0778B5");
+  rect(200, 250, 100, 100);
+  pop();
+}
+
+function plus() {
+  push();
+  drawingContext.shadowBlur = 12;
+  drawingContext.shadowOffsetX = 0;
+  drawingContext.shadowOffsetY = 0;
+  drawingContext.shadowColor = "#05050550";
+  noStroke();
+  fill("#80E82F");
+  rect(191, 280, 118, 40);
+  pop();
+}
+
+function feck() {
+  push();
+  noStroke();
+  fill("#80E82F");
+  rect(230, 240, 40, 118);
+  pop();
+}
+
+function playButton() {
+  push();
+  drawingContext.shadowBlur = 6;
+  drawingContext.shadowOffsetX = 0;
+  drawingContext.shadowOffsetY = 0;
+  drawingContext.shadowColor = "#05050550";
+  noStroke();
+  fill("white");
+  triangle(240, 270, 240, 330, 280, 300);
+  pop();
+}
+
 function draw() {
   background("#10164E");
   finishText();
+  bruh();
+  bruhx();
+  btns();
+  plus();
+  feck();
+  playButton();
 }
